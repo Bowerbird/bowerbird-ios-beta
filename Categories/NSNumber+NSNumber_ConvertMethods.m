e469c506545855bfc2ae4735a7a7b08a882f2b30
Hamish Crittenden <hamish.crittenden@gmail.com>
2012-08-09 11:01:51 +1000

    Updated to physical folder located

---
+++ Categories/NSNumber+NSNumber_ConvertMethods.m
@@ -0,0 +1,18 @@
+//
+//  NSNumber+NSNumber_ConvertMethods.m
+//  BowerBird
+//
+//  Created by Hamish Crittenden on 7/08/12.
+//  Copyright (c) 2012 Museum Victoria. All rights reserved.
+//
+
+#import "NSNumber+NSNumber_ConvertMethods.h"
+
+@implementation NSNumber (ConvertMethods)
+
++(NSNumber *)ConvertFromString: (NSString *)stringNumber
+{
+    return [NSNumber numberWithInteger:[stringNumber integerValue]];
+}
+
+@end

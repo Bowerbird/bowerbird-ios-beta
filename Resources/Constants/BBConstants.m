90fad83631dbaa8e097f088bb4acdc7958209a28
Hamish Crittenden <hamish.crittenden@gmail.com>
2012-08-21 03:41:32 +1000

    RK refactor

---
+++ Resources/Constants/BBConstants.m
@@ -0,0 +1,101 @@
+/*-----------------------------------------------------------------------------------------------
+ 
+ BowerBird V1 - Licensed under MIT 1.1 Public License
+ Developers: Frank Radocaj : frank@radocaj.com, Hamish Crittenden : hamish.crittenden@gmail.com
+ Project Manager: Ken Walker : kwalker@museum.vic.gov.au
+ 
+ -----------------------------------------------------------------------------------------------*/
+
+#import "BBConstants.h"
+
+@implementation BBConstants
+
+// colors
++(NSString *)BlueHexString
+{
+    return @"#16255B";
+}
+
++(NSString *)PurpleHexString
+{
+    return @"#606a85";
+}
+
++(BOOL) Trace
+{
+    return YES;
+}
+
+// url
++(NSURL *)RootUri
+{
+    return [NSURL URLWithString:[self RootUriString]];
+}
+
++(NSString *)RootUriString
+{
+    // Hamish's PC
+    return @"http://dev.bowerbird.org.au";
+    
+    // Bowerbird dev server
+    //return @"http://136.154.22.24:65000";
+}
+
+// restful segments
++(NSURL *)ProjectsUrl
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", [self RootUriString], @"/projects"]];
+}
+
+// restful segments
++(NSURL *)ActivityUrl
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", [self RootUriString], @"/account/activity"]];
+}
+
++(NSURL *)AccountLoginUrl
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", [self RootUriString], @"/account/login"]];
+}
+
++(NSURL *)AuthenticatedUserProfileUrl
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", [self RootUriString], @"/account/profile"]];
+}
+
++(NSString *)AuthCookieName
+{
+    return @"ASP.NET_SessionId";
+}
+
++(NSArray*)AvatarTypes
+{
+    return [NSArray arrayWithObjects: @"Original", @"Square42", @"Square100", @"Square200", nil];
+}
+
++(NSArray*)MediaImageTypes
+{
+    return [NSArray arrayWithObjects:@"Original",@"Square42", @"Square100", @"Square200",@"Full480",@"Full768",@"Full1024", nil];
+}
+
++(NSString *)NameOfAvatarDisplayImage
+{
+    return @"Square200";
+}
+
++(NSString*)NameOfMediaResourceDisplayImage
+{
+    return @"Full768";
+}
+
++(NSString *)CookieName
+{
+    return @".BOWERBIRDAUTH";
+}
+
++(NSDictionary*)AjaxRequestParams
+{
+    return [[NSDictionary alloc]initWithObjectsAndKeys:@"XMLHttpRequest",@"X-Requested-With", nil];
+}
+
+@end

bf7db53566e7f7f67c11a573bdda7c3a9f836882
Hamish Crittenden <hamish.crittenden@gmail.com>
2012-11-09 12:13:33 +1100

    Massive refactor.

---
+++ Classes/Helpers/BBSizes.m
@@ -0,0 +1,13 @@
+//
+//  BBSizes.m
+//  BowerBird
+//
+//  Created by Hamish Crittenden on 9/10/12.
+//  Copyright (c) 2012 BowerBird. All rights reserved.
+//
+
+#import "BBSizes.h"
+
+@implementation BBSizes
+
+@end
---
+++ Classes/Models/BBChatDetails.m
@@ -0,0 +1,13 @@
+//
+//  BBChatDetails.m
+//  BowerBird Beta
+//
+//  Created by Hamish Crittenden on 28/09/12.
+//  Copyright (c) 2012 Museum Victoria. All rights reserved.
+//
+
+#import "BBChatDetails.h"
+
+@implementation BBChatDetails
+
+@end

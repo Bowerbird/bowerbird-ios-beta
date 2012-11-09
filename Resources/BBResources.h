9b0fef644c4345b51561e0621d7033f748669d7b
Hamish Crittenden <hamish.crittenden@gmail.com>
2012-11-09 12:13:54 +1100

    Massive refactor.

--- Resources/BBResources.h
+++
@@ -1,9 +0,0 @@
-//
-//  BBResources.h
-//  BowerBird Beta
-//
-//  Created by Hamish Crittenden on 17/08/12.
-//  Copyright (c) 2012 Museum Victoria. All rights reserved.
-//
-
-#import "BBConstants.h"
